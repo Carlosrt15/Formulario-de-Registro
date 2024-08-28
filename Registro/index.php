e19f3d2952675bfeaf852f993c837c3feb4456de
Carlos Ródriguez Talegon <132402218+Carlosrt15@users.noreply.github.com>
2024-08-28 16:35:18 +0200

    Add files via upload

---
+++ Registro/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    
+    <head>
+
+    <meta charset ="UTF-8">
+    <meta name="viewport"   content="width=device-width, initial-scale=1.0">
+    <meta http-equiv = "X-UA-Compatible" content="IE=edge">
+    <title>Registro</title>
+    <link rel="stylesheet" href="style.css">
+
+    </head>
+
+    <body>
+
+    <form method ="post">
+
+    <h2>Hola</h2>
+    <p>Inicia el Registro </p>
+
+    <div class="input-wrapper">
+        <input type="text" name="name" placeholder="Nombre">
+        <img class="input-icon"   src="images/name.svg" alt="">
+</div>
+<br>
+<div class="input-wrapper">
+        <input type="email" name="email" placeholder="Email">
+        <img class="input-icon"   src="images/email.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+        <input type="text" name="direction" placeholder="Direccíon">
+        <img class="input-icon"   src="images/direction.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+        <input type="tel" name="phone" placeholder="Telefono">
+        <img class="input-icon"   src="images/phone.svg" alt="">
+</div>
+
+<div class="input-wrapper">
+        <input type="password" name="password" placeholder="Contraseña">
+        <img class="input-icon"   src="images/password.svg" alt="">
+</div>
+
+    <input class="btn" type="submit" name="register" value="Enviar">
+
+    </form>
+
+    <?php 
+        include("registrar.php");
+    ?>
+
+
+    </body>
+
+
+
+
+
+    </html>
